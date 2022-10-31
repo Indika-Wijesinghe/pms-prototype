0e4815da4b522c52158afd014bfdfa9fbfbe8eab
Indika Wijesinghe <indikawijesinghe243@gmail.com>
2022-10-31 16:11:08 +0530

    deploy prototype

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -13,7 +13,24 @@ import {
 
 import { employeesData, employeesGrid } from "../data/dummy";
 import { Header } from "../components";
+import { NavLink } from "react-router-dom";
 
-const Dashboard = () => {};
+const Dashboard = () => {
+  return (
+    <>
+      <div className="m-2 md:m-10 p-2 md:p-10 bg-[#fafafa] rounded-md flex justify-start ">
+        <div>
+          <div className="">
+            <NavLink to={`/create-a-booking`}>
+              <button className="text-md text-white pt-[6px] px-4 py-1 rounded-lg bg-[#0f172a]">
+                Create a booking
+              </button>
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Dashboard;
