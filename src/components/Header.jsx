548af64040408e53f2dc0b4f2271e3fb1fd620f6
Indika Wijesinghe <indikawijesinghe243@gmail.com>
2022-10-31 16:32:55 +0530

    deploy prototype

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({ category, title }) => {
   return (
     <div className="mb-10">
       <p className="text-gray-400">{category}</p>
-      <p className="text-3xl tracking-tight font-medium text-slate-900">
+      <p className="text-3xl tracking-tight font-semibold text-slate-900">
         {title}
       </p>
     </div>
